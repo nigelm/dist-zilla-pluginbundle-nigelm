96489e0dabad37e3a44dfb7384e4aa8486da3d22
Nigel Metheringham <nigelm@cpan.org>
2011-02-05 15:18:18 +0000

    Set bundle up appropriately
    Had issues in trying to just extend methods, so have gone
    to overriding methods completely.
    Added documentation - also pulled from FLORA

--- lib/Dist/Zilla/PluginBundle/NIGELM.pm
+++ lib/Dist/Zilla/PluginBundle/NIGELM.pm
@@ -3,43 +3,136 @@ package Dist::Zilla::PluginBundle::NIGELM;
 # ABSTRACT: Build your distributions like FLORA does with NIGELM tweaks
 
 use Moose 1.00;
+use Method::Signatures::Simple;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::URI qw(Uri);
+use MooseX::Types::Email qw(EmailAddress);
+use MooseX::Types::Moose qw(Bool Str CodeRef);
+use MooseX::Types::Structured 0.20 qw(Map Dict Optional);
 use namespace::autoclean -also => 'lower';
 
 extends 'Dist::Zilla::PluginBundle::FLORA';
-with 'Dist::Zilla::Role::PluginBundle::Easy';
 
-# update FLORA's defaults to suit my modules...
-has '+repository_at' => default => github );
+=head1 SYNOPSIS
 
-has '+authority' => ( default => 'cpan:NIGELM', );
+In dist.ini:
 
-has '+github_user' => ( default => 'nigelm', );
+  [@NIGELM]
+  dist = Distribution-Name
+  repository_at = github
+
+=head1 DESCRIPTION
+
+This is the L<Dist::Zilla> configuration I use to build my
+distributions. It is strongly based on the @FLORA bundle, and
+extends that bundle (so if that bundle is changed incompatibly I
+get to keep both pieces)
+
+It is roughly equivalent to:
+
+  [@Filter]
+  bundle = @Basic
+
+  [MetaConfig]
+  [MetaJSON]
+  [PkgVersion]
+  [PodSyntaxTests]
+  [PodCoverageTests]
+  [NoTabsTests]
+  [EOLTests]
+  [ReadmeFromPod]
+
+  [MetaResources]
+  repository.type   = git
+  repository.url    = git://github.com/nigelm/${lowercase_dist}
+  repository.web    = http://github.com/nigelm/${lowercase_dist}
+  bugtracker.web    = http://rt.cpan.org/Public/Dist/Display.html?Name=${dist}
+  bugtracker.mailto = bug-${dist}@rt.cpan.org
+  homepage          = http://search.cpan.org/dist/${dist}
+
+  [Authority]
+  authority   = cpan:NIGELM
+  do_metadata = 1
+
+  [@Git]
+
+  [PodWeaver]
+  config_plugin = @FLORA
+
+  [AutoPrereqs]
+
+=cut
+
+has authority => (
+    is      => 'ro',
+    isa     => Str,
+    default => 'cpan:NIGELM',
+);
 
 has 'tag_format' => (
-      is      => 'ro',
-      isa     => Str,
-      default => 'release/%v',
-  );
+    is      => 'ro',
+    isa     => Str,
+    default => 'release/%v',
+);
 
 has 'tag_message' => (
-      is      => 'ro',
-      isa     => Str,
-      default => 'Release of %v',
-  );
+    is      => 'ro',
+    isa     => Str,
+    default => 'Release of %v',
+);
 
-# change the configure method to add more bundles...
-after configure => sub {
-      my $self = shift;
+method configure {
+    $self->add_bundle('@Basic');
 
-      $self->add_plugins('ReadmeFromPod');
-      $self->add_bundle(
-          '@Git' => {
-              tag_format  => $self->tag_format,
-              tag_message => $self->tag_message,
-          }
-      );
+    $self->add_plugins(
+        qw(
+          MetaConfig
+          MetaJSON
+          PkgVersion
+          PodSyntaxTests
+          PodCoverageTests
+          NoTabsTests
+          EOLTests
+          ReadmeFromPod
+          )
+    );
 
-  };
+    $self->add_plugins('PodCoverageTests')
+      unless $self->disable_pod_coverage_tests;
+
+    $self->add_plugins(
+        [
+            MetaResources => {
+                'repository.type'   => $self->repository_type,
+                'repository.url'    => $self->repository_url,
+                'repository.web'    => $self->repository_web,
+                'bugtracker.web'    => $self->bugtracker_url,
+                'bugtracker.mailto' => $self->bugtracker_email,
+                'homepage'          => $self->homepage_url,
+            }
+        ],
+        [
+            Authority => {
+                authority   => $self->authority,
+                do_metadata => 1,
+            }
+        ],
+        [ EOLTests => { trailing_whitespace => !$self->disable_trailing_whitespace_tests, } ],
+    );
+
+    $self->add_bundle(
+        '@Git' => {
+            tag_format  => $self->tag_format,
+            tag_message => $self->tag_message,
+        }
+    );
+
+    $self->is_task
+      ? $self->add_plugins('TaskWeaver')
+      : $self->add_plugins( [ PodWeaver => { config_plugin => $self->weaver_config_plugin, } ], );
+
+    $self->add_plugins('AutoPrereq') if $self->auto_prereq;
+}
 
 __PACKAGE__->meta->make_immutable;
 
