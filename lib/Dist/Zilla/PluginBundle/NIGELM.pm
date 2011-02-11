78b9a4d0943ee44ec70eb98b8aa7d6ad3e322cd0
Nigel Metheringham <nigelm@cpan.org>
2011-02-11 11:28:29 +0000

    Change git handling to deal with README

--- lib/Dist/Zilla/PluginBundle/NIGELM.pm
+++ lib/Dist/Zilla/PluginBundle/NIGELM.pm
@@ -493,7 +493,7 @@ method configure () {
               ]
             : ()
         ),
-        [ 'Git::Check' => {} ],
+        [ 'Git::Check' => [ allow_dirty => 'dist.ini', allow_dirty => 'Changes', allow_dirty => 'README', ] ],
 
         # -- fetch & generate files
         [ GatherDir    => {} ],
@@ -579,7 +579,7 @@ method configure () {
 
         # -- Git release process
         [ CopyReadmeFromBuild => {} ],
-        [ 'Git::Commit'       => {} ],
+        [ 'Git::Commit'       => [ allow_dirty => 'dist.ini', allow_dirty => 'Changes', allow_dirty => 'README', ] ],
         [
             'Git::Tag' => {
                 tag_format  => $self->tag_format,
