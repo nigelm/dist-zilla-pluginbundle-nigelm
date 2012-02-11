fab5142daddcfc5f7bec6a29a29173231da58982
Nigel Metheringham <nigelm@cpan.org>
2012-02-11 22:10:54 +0000

    Formatting fixes - no code changes

--- lib/Dist/Zilla/PluginBundle/NIGELM.pm
+++ lib/Dist/Zilla/PluginBundle/NIGELM.pm
@@ -263,9 +263,9 @@ has is_task => (
     builder => '_build_is_task',
 );
 
-method _build_is_task() {
+method _build_is_task () {
     return $self->dist =~ /^Task-/ ? 1 : 0;
-    }
+}
 
 =head3 weaver_config_plugin
 
@@ -275,11 +275,11 @@ loads in L<Pod::Weaver::PluginBundle::MARCEL>.
 
 =cut
 
-    has weaver_config_plugin => (
+has weaver_config_plugin => (
     is      => 'ro',
     isa     => Str,
     default => '@MARCEL',
-    );
+);
 
 =head2 Bug Tracker Information
 
@@ -298,9 +298,9 @@ has bugtracker_url => (
     handles => { bugtracker_url => 'as_string', },
 );
 
-method _build_bugtracker_url() {
+method _build_bugtracker_url () {
     return sprintf $self->_rt_uri_pattern, $self->dist;
-    }
+}
 
 =head3 bugtracker_email
 
@@ -309,22 +309,22 @@ distribution name.
 
 =cut
 
-    has bugtracker_email => (
+has bugtracker_email => (
     is      => 'ro',
     isa     => EmailAddress,
     lazy    => 1,
     builder => '_build_bugtracker_email',
-    );
+);
 
-method _build_bugtracker_email() {
+method _build_bugtracker_email () {
     return sprintf 'bug-%s@rt.cpan.org', $self->dist;
-    }
+}
 
-    has _rt_uri_pattern => (
+has _rt_uri_pattern => (
     is      => 'ro',
     isa     => Str,
     default => 'http://rt.cpan.org/Public/Dist/Display.html?Name=%s',
-    );
+);
 
 =head2 Tweaks - Modifying Tests Generated
 
@@ -415,15 +415,15 @@ has homepage_url => (
     handles => { homepage_url => 'as_string', },
 );
 
-method _build_homepage_url() {
+method _build_homepage_url () {
     return sprintf $self->_cpansearch_pattern, $self->dist;
-    }
+}
 
-    has _cpansearch_pattern => (
+has _cpansearch_pattern => (
     is      => 'ro',
     isa     => Str,
     default => 'https://metacpan.org/release/%s',
-    );
+);
 
 =head3 repository_at
 
@@ -515,14 +515,14 @@ coerce $map_tc, from Map [
     };
     };
 
-method _build__repository_host_map() {
-    my $github_pattern         = sub { sprintf 'git://github.com/%s/%%s.git', $self->github_user };
-        my $github_web_pattern = sub { sprintf 'http://github.com/%s/%%s',    $self->github_user };
-        my $scsys_web_pattern_proto = sub {
+method _build__repository_host_map () {
+    my $github_pattern     = sub { sprintf 'git://github.com/%s/%%s.git', $self->github_user };
+    my $github_web_pattern = sub { sprintf 'http://github.com/%s/%%s',    $self->github_user };
+    my $scsys_web_pattern_proto = sub {
         return sprintf 'http://git.shadowcat.co.uk/gitweb/gitweb.cgi?p=%s/%%s.git;a=summary', $_[0];
-        };
+    };
 
-        return {
+    return {
         github => {
             pattern     => $github_pattern,
             web_pattern => $github_web_pattern,
@@ -549,24 +549,24 @@ method _build__repository_host_map() {
                     )
                 } qw(catagits p5sagit dbsrgits)
         ),
-        };
-    }
+    };
+}
 
-    method _build_repository_url() {
+method _build_repository_url () {
     return $self->_resolve_repository_with( $self->repository_at, 'pattern' )
         if $self->has_repository_at;
     confess "Cannot determine repository url without repository_at. "
         . "Please provide either repository_at or repository.";
-    }
+}
 
-    has _repository_host_map => (
+has _repository_host_map => (
     traits  => [qw(Hash)],
     isa     => $map_tc,
     coerce  => 1,
     lazy    => 1,
     builder => '_build__repository_host_map',
     handles => { _repository_data_for => 'get', },
-    );
+);
 
 =head3 repository_web
 
@@ -582,23 +582,24 @@ has repository_web => (
     handles => { repository_web => 'as_string', },
 );
 
-method _build_repository_web() {
+method _build_repository_web () {
     return $self->_resolve_repository_with( $self->repository_at, 'web_pattern' )
         if $self->has_repository_at;
     confess "Cannot determine repository web url without repository_at. "
         . "Please provide either repository_at or repository_web.";
-    }
+}
 
-    method _resolve_repository_with( $service, $thing ) {
-    my $dist     = $self->dist;
-        my $data = $self->_repository_data_for($service);
-        confess "unknown repository service $service" unless $data;
+method _resolve_repository_with ( $service, $thing ) {
+    my $dist = $self->dist;
+    my $data = $self->_repository_data_for($service);
+    confess "unknown repository service $service" unless $data;
     return sprintf $data->{$thing}->(),
-    (   exists $data->{mangle}
+        (
+        exists $data->{mangle}
         ? $data->{mangle}->($dist)
         : $dist
-    );
-    }
+        );
+}
 
 =head3 repository_type
 
@@ -606,23 +607,23 @@ The repository type - either C<svn> or C<git>.  Normally set from L<repository_a
 
 =cut
 
-    has repository_type => (
+has repository_type => (
     is      => 'ro',
     isa     => Str,
     lazy    => 1,
     builder => '_build_repository_type',
-    );
+);
 
-method _build_repository_type() {
+method _build_repository_type () {
     my $data = $self->_repository_data_for( $self->repository_at );
-        return $data->{type} if exists $data->{type};
+    return $data->{type} if exists $data->{type};
 
     for my $vcs (qw(git svn)) {
         return $vcs if $self->repository_scheme eq $vcs;
     }
 
     confess "Unable to guess repository type based on the repository url. " . "Please provide repository_type.";
-    }
+}
 
 =head3 github_user
 
@@ -631,11 +632,11 @@ useful for anyone else. Sorry!
 
 =cut
 
-    has github_user => (
+has github_user => (
     is      => 'ro',
     isa     => Str,
     default => 'nigelm',
-    );
+);
 
 =head3 tag_format / tag_message / version_regexp / git_autoversion
 
@@ -665,18 +666,18 @@ has version_regexp => (
     builder => '_build_version_regexp',
 );
 
-method _build_version_regexp() {
+method _build_version_regexp () {
     my $version_regexp = $self->tag_format;
-        $version_regexp =~ s/\%v/\(\\d+\(\?:\\.\\d+\)\+\)/;
-        $version_regexp =~ s/\%t/\(\?:\[-_\]\.+\)\?/;
-        return sprintf( '^%s$', $version_regexp );
-    }
+    $version_regexp =~ s/\%v/\(\\d+\(\?:\\.\\d+\)\+\)/;
+    $version_regexp =~ s/\%t/\(\?:\[-_\]\.+\)\?/;
+    return sprintf( '^%s$', $version_regexp );
+}
 
-    has git_autoversion => (
+has git_autoversion => (
     is      => 'ro',
     isa     => 'Bool',
     default => 1,
-    );
+);
 
 =head3 git_allow_dirty
 
@@ -713,7 +714,7 @@ override BUILDARGS => sub {
     return { %{ $args->{payload} }, %{$args} };
 };
 
-method configure() {
+method configure () {
 
     # Build a list of all the plugins we want...
     my @wanted = (
@@ -828,10 +829,10 @@ method configure() {
         ),
     );
 
-        $self->add_plugins(@wanted);
-    }
+    $self->add_plugins(@wanted);
+}
 
-    with 'Dist::Zilla::Role::PluginBundle::Easy';
+with 'Dist::Zilla::Role::PluginBundle::Easy';
 
 __PACKAGE__->meta->make_immutable;
 
